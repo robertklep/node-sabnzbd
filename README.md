9913f07130385402f8a340651c387cc19e323b95
Robert Klep <robert@klep.name>
2012-06-18 12:32:11 +0200

    Added files to repository

--- README.md
+++ README.md
@@ -1,4 +1,214 @@
 node-sabnzbd
 ============
 
-Node interface for SABnzbd
+Node interface for [SABnzbd](http://www.sabnzbd.org/).
+
+TL;DR
+-----
+
+    var SABnzbd = require('sabnzbd');
+    var sabnzbd = new SABnzbd('http://localhost:8080/', API_KEY);
+
+    console.log('Queue + History:');
+    sabnzbd.all().then(function(queue) {
+      queue.entries.forEach(function(slot) {
+        console.log('-', slot.name, ',', slot.size / 1000 / 1000, 'MB');
+      });
+    });
+
+Install
+-------
+
+* Install module first:
+    - local installation: `npm install sabnzbd`
+    - global installation: `npm install sabnzbd -g`
+* Get the API key from your SABnzbd:
+    - open SABnzbd web interface in your browser
+		- go to `Config > General`
+		- in the _SABnzbd Web Server_ settings, find the API key (or generate
+			one)
+		- note down the API key, you'll need it
+
+API
+---
+
+For the most part, the API implements the commands found on [the SABnzbd
+API page](http://wiki.sabnzbd.org/api), and returns their results pretty
+much as-is.
+
+However, because the SABnzbd API is horribly inconsistent at times, I've
+added some normalization (see `queue` and `history` commands) to make
+interfacing with it a bit easier.
+
+SABnzbd has two main lists: the queue and the history. The queue contains
+all active downloads (the ones that haven't finished yet), the history
+contains all completed downloads.
+
+`sabnzbd` uses [Kris Kowal's `q` library](https://github.com/kriskowal/q),
+which means that most commands return a promise.
+
+### `new SABnzbd(URL, API_KEY)`
+
+Connects to SABnzbd. It will automatically perform a quick check to
+determine the SABnzbd version and to see if your API key is valid.
+
+Arguments:
+
+* `URL`: url to web interface of the SABnzbd
+* `API_KEY`: API key (required for most operations, see _Install_ on how to
+	get it)
+
+Returns:
+
+* an `SABnzbd` instance
+
+### `instance.queue()`
+
+Get contents of the SABnzbd queue.
+
+Returns the output of the [advanced queue command](http://wiki.sabnzbd.org/api#toc8),
+with an extra property `entries` containing a normalized version of the `slots` property:
+
+An entry contains the following properties:
+
+    age         : age of NZB posting, in seconds
+    size        : size of download in bytes
+    size_left   : number of bytes still to download before completion
+    nzbid       : internal SABnzbd id for this NZB
+    category    : categories
+    eta         : ETA for download, as Date object
+    name        : NZB filename
+    nzbname     : NZB filename
+    percentage  : percentage downloaded
+    index       : index into queue
+    missing     : ?
+    priority    : ?
+    status      : download status ('Completed', 'Paused', 'Queued,
+                  'Failed', 'Verifying', 'Downloading', 'Extracting')
+    time_left   : time left before download should be complete, in seconds
+
+### `instance.history()`
+
+Get contents of the SABnzbd history. Returns the output of the [history
+command](http://wiki.sabnzbd.org/api#toc11), with, again, an extra
+`entries` property:
+
+    action_line    : ?
+    size           : size in bytes
+    category       : categories
+    completed      : completed timestamp, as Date object
+    completeness   : ?
+    download_time  : download time in seconds
+    downloaded     : number of downloaded bytes
+    fail_message   : message why download failed
+    id             : internal id (not the same as `nzbid`)
+    loaded         : ?
+    meta           : ?
+    name           : name of download
+    nzbname        : NZB filename
+    nzbid          : internal SABnzbd id for this NZB
+    incomplete_path: path where SABnzbd stored incomplete download
+    postproc_time  : time in seconds it took to postprocess this NZB
+    pp             : ?
+    report         : ?
+    retry          : ?
+    script         : ?
+    script_line    : ?
+    script_log     : ?
+    show_details   : ?
+    stage_log      : list of actions taken by SABnzbd to download/process
+                     this NZB
+    status         : status (see above)
+    downloaded_to  : file/directory this NZB was downloaded to
+    url            : ?
+    url_info       : ?
+
+### `instance.all()`
+
+Returns the contents of both the `queue` and `history` commands combined
+(**NB**: for now, only the `slots` and `entries` are actually merged, the
+rest of the object returned is based object returned by the `queue` command).
+
+### `instance.addurl(URL)`
+
+Add an NZB to the queue by URL.
+
+Arguments:
+
+* `URL`: url pointing to an NZB file
+
+### `instance.pause(ID)`
+
+Pause downloading of an NZB.
+
+Arguments:
+
+* `ID`: id of NZB (the `nzbid` property of queue/history entries)
+
+### `instance.stop(ID)`
+
+Alias for `instance.pause(ID)`
+
+### `instance.resume(ID)`
+
+Resume/start downloading of an NZB.
+
+Arguments:
+
+* `ID`: id of NZB (the `nzbid` property of queue/history entries)
+
+### `instance.start(ID)`
+
+Alias for `instance.resume(ID)`
+
+### `instance.queue_remove(ID)`
+
+Remove an NZB from the queue.
+
+Arguments:
+
+* `ID`: id of NZB (the `nzbid` property of queue/history entries)
+
+### `instance.history_remove(ID)`
+
+Remove an NZB from the history.
+
+Arguments:
+
+* `ID`: id of NZB (the `nzbid` property of queue/history entries)
+
+### `instance.remove(ID)`
+
+Remove an NZB from both queue and/or history.
+
+## Internal API
+
+### `instance.cmd(CMD[, ARGS])`
+
+Send a command to the SABnzbd.
+
+Arguments:
+
+* `CMD`: command to send
+* `ARGS`: optional object of _key/value_ parameters
+
+(for all commands and their arguments, check the [the SABnzbd
+API page](http://wiki.sabnzbd.org/api))
+
+For example, the `instance.addurl()` method is implemented as such:
+
+    return this.cmd('addurl', { name : url });
+
+TODO
+----
+
+* Queue:
+    - Add by upload/file path/newzbin ID
+    - Scripts/actions/priority
+    - Shutdown
+    - Move
+    - Change item name
+* History:
+    - Retry
+* Configuration:
+    - Everything
---
+++ sabnzbd.js
@@ -0,0 +1,242 @@
+(function() {
+	var request		= require('request');
+	var URL				= require('url');
+	var util			= require('util');
+	var QS				= require('querystring');
+	var Q					= require('q');
+	var dateutil	= require('dateutil');
+
+	// dateutil parser for ETA's
+	dateutil._parsers['sabnzbd'] = {
+		test  : /^\s*[\d:]+\s+[a-zA-Z]{3}\s+\d+\s+[a-zA-Z]{3}\s*$/,
+		parse : function(str) {
+			var m = str.match(/^([\d:]+)\s+(.*)/);
+			var t = [ m[2], (new Date()).getFullYear(), m[1] ].join(" ");
+			return dateutil.parse(t);
+		}
+	};
+
+	// SABnzbd class
+	var SABnzbd = function(url, apikey, nzbkey) {
+		this.apikey = apikey;
+		this.nzbkey	= nzbkey || apikey;
+
+		// attach API endpoint to url
+		if (url.indexOf('/sabnzbd/api') == -1)
+			url = url.replace(/\/?$/, '/sabnzbd/api');
+		this.url = url;
+
+		// check for valid endpoint
+		this.version().then(function(version) {
+			util.log('SABnzbd version: ' + version);
+		});
+
+		// check for valid API key
+		this.cmd('').then(function(response) {
+			if (response.indexOf('API Key Incorrect') != -1)
+				util.log("Supplied API key is invalid, things will probably start failing.");
+			else
+				util.log('SABnzbd accepted supplied API key.');
+		});
+	};
+
+	// perform command request
+	SABnzbd.prototype.cmd = function(command, args) {
+		// build url for request
+		var url		= this.url + '?' + QS.stringify({
+			mode	: command,
+			apikey: this.apikey,
+			output: 'json'
+		});
+
+		// tack on any passed arguments
+		if (args)
+			url += '&' + QS.stringify(args);
+
+		//console.log('RETR', url);
+
+		// perform request
+		var defer = Q.defer();
+		request.get(url, function(error, response, body) {
+			if (error)
+				defer.reject(error);
+			else
+			{
+				// JSON response?
+				if (response.headers['content-type'].indexOf('application/json') != -1)
+					body = JSON.parse(body);
+				defer.resolve(body);
+			}
+		});
+
+		// return deferred object
+		return defer.promise;
+	};
+
+	// get server version
+	SABnzbd.prototype.version = function() {
+		return this.cmd('version').then(function(version) {
+			return version.replace(/^\s+|\s+$/, '');
+		});
+	};
+
+	// get queue contents
+	SABnzbd.prototype.queue = function() {
+		var _this = this;
+		return this.cmd('queue').then(function(response) {
+			var queue = response.queue || { slots: [] };
+
+			// normalize queue slots
+			var slots	= queue.slots.map(function(slot) {
+				return _this.normalize_queue(slot);
+			});
+			queue.entries = slots;
+
+			// done
+			return queue;
+		});
+	};
+
+	// get history contents
+	SABnzbd.prototype.history = function() {
+		var _this = this;
+
+		return this.cmd('history').then(function(response) {
+			var history = response.history || { slots: [] };
+
+			// normalize history slots
+			var slots	= history.slots.map(function(slot) {
+				return _this.normalize_history(slot);
+			});
+			history.entries = slots;
+
+			// done
+			return history;
+		});
+	};
+
+	// get both queue and history contents, merged
+	SABnzbd.prototype.all = function() {
+		return Q.all([ this.queue(), this.history() ]).spread(function(queue, history) {
+			// merge slots
+			if (! queue.slots) queue.slots = [];
+			history.slots.forEach(function(slot) {
+				queue.slots.push(slot);
+			});
+
+			// merge entries
+			if (! queue.entries) queue.entries = [];
+			history.entries.forEach(function(entry) {
+				queue.entries.push(entry);
+			});
+
+			return queue;
+		});
+	};
+
+	// add an NZB url to the queue
+	SABnzbd.prototype.addurl = function(url) {
+		return this.cmd('addurl', { name : url });
+	};
+
+	// pause/stop download of item
+	SABnzbd.prototype.pause = function(id) {
+		return this.cmd('queue', { name : 'pause', value : id });
+	};
+	SABnzbd.prototype.stop = SABnzbd.prototype.pause;
+
+	// resume/start download of item
+	SABnzbd.prototype.resume = function(id) {
+		return this.cmd('queue', { name : 'resume', value : id });
+	};
+	SABnzbd.prototype.start = SABnzbd.prototype.resume;
+
+	// remove an item from the queue
+	SABnzbd.prototype.queue_remove = function(id) {
+		return this.cmd('queue', { name : 'delete', value : id });
+	};
+
+	// remove an item from the history
+	SABnzbd.prototype.history_remove = function(id) {
+		return this.cmd('history', { name : 'delete', value : id });
+	};
+
+	// remove an item all together
+	SABnzbd.prototype.remove = function(id) {
+		return Q.all([
+			this.queue_remove(id),
+			this.history_remove(id),
+		]);
+	};
+
+	// normalize queue slot
+	SABnzbd.prototype.normalize_queue = function(slot) {
+		// parse timeleft
+		var timeleft = slot.timeleft.replace(/^(\d+):(\d+):(\d+)/, function(all, H, M, S) {
+			return parseInt(H) * 3600 + parseInt(M) * 60 + parseInt(S);
+		});
+
+		// parse ETA
+		var eta = slot.eta == 'unknown' ? slot.eta : dateutil.parse(slot.eta);
+
+		// parse age
+		var age	= slot.avg_age.replace(/^\s*(\d+).*/, function(a, H) {
+			return parseInt(H) * 3600;
+		});
+
+		// return a normalized object
+		return {
+			age					: parseInt(age),
+			size				: slot.mb * 1000 * 1000,
+			size_left		: slot.mbleft * 1000 * 1000,
+			nzbid				: slot.nzo_id,
+			category		: slot.cat,
+			eta					: eta,
+			name				: slot.filename,
+			nzbname			: slot.filename,
+			percentage	: parseInt(slot.percentage),
+			index				: slot.index,
+			missing			: slot.missing,
+			priority		: slot.priority,
+			status			: slot.status,
+			time_left		: parseInt(timeleft)
+		};
+	};
+
+	// normalize history slot
+	SABnzbd.prototype.normalize_history = function(slot) {
+		return {
+			action_line		: slot.action_line,
+			size					: slot.bytes,
+			category			: slot.category,
+			completed			: new Date(slot.completed * 1000.0),
+			completeness	: slot.completeness,
+			download_time	: slot.download_time,
+			downloaded		: slot.downloaded,
+			fail_message	: slot.fail_message,
+			id						: slot.id,
+			loaded				: slot.loaded,
+			meta					: slot.meta,
+			name					: slot.name,
+			nzbname				: slot.nzb_name,
+			nzbid					: slot.nzo_id,
+			incomplete_path: slot.path,
+			postproc_time : slot.postproc_time,
+			pp						: slot.pp,
+			report				: slot.report,
+			retry					: slot.retry,
+			script				: slot.script,
+			script_line		: slot.script_line,
+			script_log		: slot.script_log,
+			show_details	: slot.show_details == "True",
+			stage_log			: slot.stage_log,
+			status				: slot.status,
+			downloaded_to	: slot.storage,
+			url						: slot.url,
+			url_info			: slot.url_info
+		};
+	};
+
+	// done
+	module.exports = SABnzbd;
+})();
